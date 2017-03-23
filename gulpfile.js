696044e0e1773576dd65bca03e88069f553b7089
mattlaguardia <matthewlaguardia@gmail.com>
2017-03-22 18:14:58 -0700

    updates gulpfile with minify task

---
+++ gulpfile.js
@@ -0,0 +1,13 @@
+var gulp = require('gulp')
+var gutil = require('gulp-util')
+var cleanCSS = require('gulp-clean-css')
+
+gulp.task('default', function() {
+	return gutil.log('Gulp is running!')
+})
+
+gulp.task('minify-css', function() {
+  return gulp.src('css/*.css')
+    .pipe(cleanCSS({compatibility: 'ie8'}))
+    .pipe(gulp.dest('css'));
+});
